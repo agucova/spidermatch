84b80c4fc8c37f0b91079a7344790aea0d19bb1a
Agustín Covarrubias <agucova@uc.cl>
2022-04-02 15:55:10 -0300

    feat: rule and site management

--- spidermatch/spidermatch.py
+++ spidermatch/spidermatch.py
@@ -6,6 +6,7 @@ import csv
 from PyQt6 import QtWidgets, uic
 from qt_material import apply_stylesheet
 
+
 class WelcomeWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(WelcomeWindow, self).__init__()
@@ -27,92 +28,142 @@ class WelcomeWindow(QtWidgets.QMainWindow):
             self.api_token_input.setPlaceholderText("Token inválido")
             QtWidgets.QMessageBox.warning(self, "Error", "Token inválido")
 
+
 class PanelWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(PanelWindow, self).__init__()
 
         # Site and rule list
-        # TODO: Solve undefinition error
-        self._site_list: list[str] = []
-        self._rule_list: list[str] = []
+        self.site_list: list[str] = []
+        self.rule_list: list[str] = []
 
         # Load the UI
         uic.loadUi("windows/panel.ui", self)
 
-        # Models
-        # self.site_list_model = QStandardItemModel(self.site_list_view)
-        # self.rule_list_model = QStandardItemModel(self.rule_list_view)
-        # self.site_list_view.setModel(self.site_list_model)
-        # self.rule_list_view.setModel(self.rule_list_model)
-
         # Site and rule list signals
         self.site_import_button.clicked.connect(self.import_sites)
         self.site_export_button.clicked.connect(self.export_sites)
+        self.site_add_button.clicked.connect(self.add_site)
+        self.site_delete_button.clicked.connect(self.delete_site)
         self.rule_import_button.clicked.connect(self.import_rules)
         self.rule_export_button.clicked.connect(self.export_rules)
+        self.rule_add_button.clicked.connect(self.add_rule)
+        self.rule_delete_button.clicked.connect(self.delete_rule)
 
+        # Domain validation regex
+        self.domain_validator = re.compile(
+            r"\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b$"
+        )
 
         self.show()
-
-    @property
-    def site_list(self):
-        return self._site_list
-
-    @site_list.setter
-    def site_list(self, site_list: list[str]):
-        self._site_list = site_list
-        self.update_site_list()
-
-    @property
-    def rule_list(self):
-        return self._rule_list
-
-    @rule_list.setter
-    def rule_list(self, rule_list: list[str]):
-        self._rule_list = rule_list
-        self.update_rule_list()
 
     def update_site_list(self):
         self.site_list_view.clear()
         self.site_list_view.addItems(self.site_list)
-        self.site_list_view.update()
 
     def update_rule_list(self):
         self.rule_list_view.clear()
-        self.rule_list_view.addItems(self.site_list)
-        self.rule_list_view.update()
+        self.rule_list_view.addItems(self.rule_list)
 
     def import_sites(self):
         # TODO: Add dialogue validation
-        file_path, check = QtWidgets.QFileDialog.getOpenFileName(self, "Importar sitios", "", "CSV (*.csv)")
-        with open(file_path, "r") as f:
-            reader = csv.reader(f)
-            for row in reader:
-                # TODO: Define input format
-                self.site_list.append(row[0])
-        self.update_site_list()
+        file_path, check = QtWidgets.QFileDialog.getOpenFileName(
+            self, "Importar sitios", "", "CSV (*.csv)"
+        )
+        if check:
+            with open(file_path, "r") as f:
+                reader = csv.reader(f)
+                errors = 0
+                for i, row in enumerate(reader):
+                    # TODO: Define input format
+                    if self.domain_validator.match(row[0]):
+                        self.site_list.append(row[0])
+                    else:
+                        QtWidgets.QMessageBox.warning(
+                            self,
+                            "Advertencia",
+                            f"Dominio inválido en la línea {i} (saltado). Recuerda no usar http:// o https:// antes del dominio.",
+                        )
+                        errors += 1
+                        if errors >= 3:
+                            QtWidgets.QMessageBox.warning(
+                                self,
+                                "Error",
+                                "Demasiados errores. Cancelando importación.",
+                            )
+                            break
+            self.update_site_list()
 
     def export_sites(self):
-        file_path, check = QtWidgets.QFileDialog.getSaveFileName(self, "Exportar sitios", "", "CSV (*.csv)")
-        with open(file_path, "w") as f:
-            writer = csv.writer(f)
-            writer.writerows(self.site_list)
+        file_path, check = QtWidgets.QFileDialog.getSaveFileName(
+            self, "Exportar sitios", "", "CSV (*.csv)"
+        )
+        if check:
+            with open(file_path, "w") as f:
+                writer = csv.writer(f)
+                writer.writerows(self.site_list)
+
+    def add_site(self):
+        site_name, ok = QtWidgets.QInputDialog.getText(
+            self, "Agregar sitio", "Dominio:"
+        )
+        if ok:
+            # From the Regular Expressions Cookbook, 2nd Edition by Jan Goyvaerts, Steven Levithan
+            if self.domain_validator.match(site_name):
+                self.site_list.append(site_name)
+            else:
+                QtWidgets.QMessageBox.warning(
+                    self,
+                    "Error",
+                    "Dominio inválido. Recuerda no usar http:// o https:// antes del dominio.",
+                )
+            self.update_site_list()
+
+    def delete_site(self):
+        selected_row = self.site_list_view.currentRow()
+        if 0 <= selected_row < len(self.site_list):
+            self.site_list.pop(selected_row)
+            self.update_site_list()
+        else:
+            QtWidgets.QMessageBox.warning(
+                self, "Error", "No hay sitio seleccionado para eliminar."
+            )
 
     def import_rules(self):
-        file_path, check = QtWidgets.QFileDialog.getOpenFileName(self, "Importar reglas", "", "CSV (*.csv)")
-        with open(file_path, "r") as f:
-            reader = csv.reader(f)
-            for row in reader:
-                self.rule_list.append(row[0])
-        self.update_rule_list()
+        file_path, check = QtWidgets.QFileDialog.getOpenFileName(
+            self, "Importar reglas", "", "CSV (*.csv)"
+        )
+        if check:
+            with open(file_path, "r") as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    self.rule_list.append(row[0])
+            self.update_rule_list()
 
     def export_rules(self):
-        file_path, check = QtWidgets.QFileDialog.getSaveFileName(self, "Exportar reglas", "", "CSV (*.csv)")
-        with open(file_path, "w") as f:
-            writer = csv.writer(f)
-            writer.writerows(self.rule_list)
+        file_path, check = QtWidgets.QFileDialog.getSaveFileName(
+            self, "Exportar reglas", "", "CSV (*.csv)"
+        )
+        if check:
+            with open(file_path, "w") as f:
+                writer = csv.writer(f)
+                writer.writerows(self.rule_list)
 
+    def add_rule(self):
+        rule_name, ok = QtWidgets.QInputDialog.getText(self, "Agregar regla", "Regla:")
+        if ok and rule_name:
+            self.rule_list.append(rule_name)
+            self.update_rule_list()
 
+    def delete_rule(self):
+        selected_row = self.rule_list_view.currentRow()
+        if 0 <= selected_row < len(self.rule_list):
+            self.rule_list.pop(selected_row)
+            self.update_rule_list()
+        else:
+            QtWidgets.QMessageBox.warning(
+                self, "Error", "No hay regla seleccionada para eliminar."
+            )
 
 
 if __name__ == "__main__":
